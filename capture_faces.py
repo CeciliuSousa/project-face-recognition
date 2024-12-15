2bf4beba6a9b7b2fdc2ed90b9447882e86435de1
Cecilius Afonso Sousa do Carmo <ceciliusousa@gmail.com>
2024-12-15 18:09:35 -0300

    Aprimorando o capture faces

--- capture_faces.py
+++ capture_faces.py
@@ -1,19 +1,23 @@
 import os
 import time
-
 import cv2
 
-# Configurar pasta para salvar as imagens
-output_folder = "imagens/outros"
+# Configurar diretório base
+base_folder = "imagens"
+os.makedirs(base_folder, exist_ok=True)
+
+# Solicitar o nome da pasta
+nome_pasta = input("Digite seu nome completo: ").title()
+output_folder = os.path.join(base_folder, nome_pasta)
 os.makedirs(output_folder, exist_ok=True)
 
-# Abrir a webcam
-cap = cv2.VideoCapture(1)
+print(f"Pasta '{nome_pasta}' criada com sucesso.")
 
-# Taxa de captura (FPS) e tempo de execução
-fps = 25
-duração = 4
-num_frames = fps * duração
+# Abrir a webcam
+cap = cv2.VideoCapture(0)  # Use 0 para a webcam padrão
+
+# Configuração de captura
+num_frames = 100
 
 print("Iniciando captura...")
 
@@ -22,11 +26,12 @@ for i in range(num_frames):
     if not ret:
         print("Erro ao capturar imagem!")
         break
-    
+
     # Nome do arquivo e salvamento
-    filename = os.path.join(output_folder, f"frame_{i:04d}.jpg")
+    filename = os.path.join(output_folder, f"Frame_{i:04d}.jpg")
+    print(filename)
     cv2.imwrite(filename, frame)
-    time.sleep(3 / fps)
+    time.sleep(3 / num_frames)
 
 print(f"Captura concluída. Imagens salvas em: {output_folder}")
 
